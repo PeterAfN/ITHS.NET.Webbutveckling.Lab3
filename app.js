41ed026ef7ebd12fb212e09cfcb9178d93b66832
peter palosaari <peterpalosaari@live.se>
2021-06-26 17:12:22 +0200

    add class removed from courses page

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ let allCourses = [];        //all courses, including bought and not bought.
 let shoppingCartItems = []; //all courses in shopping cart, not yet bought.
 
 let buy = new Buy();
-// let add = new Add("https:/localhost:5001/api/course/");
 let courses = new Courses();
 let shoppingCart = new ShoppingCart("https://localhost:5001/api/courseStudent");
 let shoppingCartBar = new ShoppingCartBar(0);
@@ -14,7 +13,6 @@ let register = new Register("https://localhost:5001/api/student/");
 
 const modalOverlayShoppingCart = document.querySelector(".modal-overlay-shopping-cart");
 const modalOverlayBuy = document.querySelector(".modal-overlay-buy");
-// const modalOverlayAdd = document.querySelector(".modal-overlay-add");
 const modalOverlayRegister = document.querySelector(".modal-overlay-register");
 
 document.addEventListener("click", (e) => {
@@ -26,8 +24,6 @@ function CloseCorrectOverlay (event) {
     shoppingCart.toggle();
   } else if (event.target === modalOverlayBuy) {
     buy.toggle();
-  // } else if (event.target === modalOverlayAdd) {
-  //   add.toggle();
   } else if (event.target === modalOverlayRegister) {
     register.toggle();
   }
